de9a5c6278c9c1dad71cd9a0fcbfa314144e1997
Tsutomu Saito <tsutomu.saito.1014@gmail.com>
2019-05-28 02:01:00 +0900

    ホーム画面だけちらっと変えた。

--- src/Home.js
+++ src/Home.js
@@ -25,7 +25,7 @@ function ComplexGrid(props) {
       <Grid container justify="center" alignItems="center" direction="row" className={classes.root}>
           <Grid className={classes.caption}>
           <Typography component="h2" variant="h5" gutterBottom>
-            Welcome to Firebase Starter Kit! 
+            Welcome to Firebase Saito Logintest 
           </Typography>
           </Grid>
       </Grid>
