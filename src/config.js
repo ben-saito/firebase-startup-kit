206c0806ef9445ffa0537aad3c9034503737d7f5
Tsutomu Saito <tsutomu.saito.1014@gmail.com>
2019-05-28 01:26:15 +0900

    自分のプロジェクトでひとまず動く様にカスタマイズ

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // Replace this section with the config file of your own Firebase project. 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8Gvm5JpOY6QlignjLSs-V2iNkQo-VVGQ",
-  authDomain: "skelton-us.firebaseapp.com",
-  databaseURL: "https://skelton-us.firebaseio.com",
-  projectId: "skelton-us",
-  storageBucket: "skelton-us.appspot.com",
-  messagingSenderId: "1041354638035",
-  appId: "1:1041354638035:web:9e02a6ea52daf17f"
+    apiKey: "AIzaSyBn3OhV42pLWXyLBECXuQZI4Io72dNDK9c",
+    authDomain: "saito-65809.firebaseapp.com",
+    databaseURL: "https://saito-65809.firebaseio.com",
+    projectId: "saito-65809",
+    storageBucket: "saito-65809.appspot.com",
+    messagingSenderId: "222371327196",
+    appId: "1:222371327196:web:e279be446847dd76"
 };
 
 export default firebaseConfig;
